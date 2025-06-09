487492027b7f6cbd73e23ceec6af7a07b716f9d6
TheSocialOG <kemel808@gmail.com>
2025-06-09 19:10:36 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Social Mint ($SMNT)</h1>
-      <p>Tu crées. Tu gagnes. Tu deviens ta propre banque.</p>
-    </main>
-  );
-}
